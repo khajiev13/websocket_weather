b2f2aea4181e081994648d68913f45bdba97f310
Roma Khajiev <raxmon1710@gmail.com>
2023-09-30 20:29:27 +0800

    Project finished

--- websocket/consumers.py
+++ websocket/consumers.py
@@ -2,17 +2,30 @@ import json
 import os
 from channels.generic.websocket import WebsocketConsumer
 
-from .data_functions import display_all_date
+from .data_functions import display_all_date, get_values_all
 
 class WSConsumer(WebsocketConsumer):
     def connect(self):
         self.accept()
-
-
         # Send JSON data with file paths
         data = display_all_date('humidity.dat')
         self.send(json.dumps({'data': data}))
 
+    def receive(self, text_data):
+        # Parse the incoming JSON message
+        message_data = 0
+        try:
+            message_data = json.loads(text_data)
+        except json.JSONDecodeError:
+            message_data = {}
+        selected_date = message_data['selected_date']
+        three_values = get_values_all(selected_date)
+        self.send(json.dumps(three_values))
+
+    def disconnect(self, close_code):
+        # Perform cleanup tasks when a client disconnects
+        # For example, you can log the disconnection
+        print(f"WebSocket connection closed with code: {close_code}")
 
 # Check if the files exist
 # if os.path.isfile('/Users/khajievroma/Desktop/Python/weather_websocket/static_data/humidity.dat'):
--- websocket/data_functions.py
+++ websocket/data_functions.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 from django.conf import settings
+import json
+
 
 def display_all_date(path):
     # Read the data file into a DataFrame
@@ -19,3 +21,36 @@ def display_all_date(path):
     
     # Return the datetime_list
     return datetime_list
+
+def get_values_all(selected_date):
+    path_light = os.path.join(settings.STATIC_ROOT, 'light.dat')
+    path_humidity = os.path.join(settings.STATIC_ROOT, 'humidity.dat')
+    path_temperature = os.path.join(settings.STATIC_ROOT, 'temperature.dat')
+    df_light = pd.read_csv(path_light, sep=" ", header=None, names=["date", "time", "value"])
+    df_temperature = pd.read_csv(path_temperature, sep=" ", header=None, names=["date", "time", "value"])
+    df_humidity = pd.read_csv(path_humidity, sep=" ", header=None, names=["date", "time", "value"])
+
+    # Combine date and time columns into a single datetime column
+    df_light["datetime"] = pd.to_datetime(df_light["date"] + " " + df_light["time"])
+    df_temperature["datetime"] = pd.to_datetime(df_temperature["date"] + " " + df_temperature["time"])
+    df_humidity["datetime"] = pd.to_datetime(df_humidity["date"] + " " + df_humidity["time"])
+    df_light["datetime"] = pd.to_datetime(df_light["date"] + " " + df_light["time"])
+
+    # Convert the datetime column to strings
+    datetime_list_light = df_light['datetime'].astype(str).to_list()
+    datetime_list_temperature = df_temperature['datetime'].astype(str).to_list()
+    datetime_list_humidity = df_humidity['datetime'].astype(str).to_list()
+
+    response_data = {
+        'humidity_value': None,
+        'light_value': None,
+        'temperature_value': None
+    }
+
+    for i in range(len(datetime_list_light)):
+        if(datetime_list_light[i] == selected_date and datetime_list_temperature[i] == selected_date and datetime_list_humidity[i] == selected_date):
+            response_data['humidity_value'] = df_humidity["value"][i].item()
+            response_data['light_value'] = df_light["value"][i].item()
+            response_data['temperature_value'] = df_temperature["value"][i].item()
+            break  # Exit the loop once values are found
+    return response_data
